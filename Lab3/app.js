2aaea7d5066b8d47225e3c41ea32be83ced55562
MrPipiKado <ammamm23102310@gmail.com>
2021-03-16 19:17:59 +0200

    Lab3 in progress

--- Lab3/app.js
+++ Lab3/app.js
@@ -11,7 +11,7 @@ function submit_form(){
     let phone = document.getElementById('phone').value
     let age = document.getElementById('age').value
 
-    validation(firstName, lastName)
+    validation(firstName, lastName, gender, email, phone, age)
 
     //let msg = `${firstName}, ${lastName}, ${gender}, ${email}, ${phone}, ${age}`
     //window.alert(msg)
@@ -30,7 +30,7 @@ function submit_form(){
 }
 
 
-function validation(firstName, lastName, gender){
+function validation(firstName, lastName, gender, email, phone, age){
 let message = ""
 if (firstName.length === 0){
     message += "First name can't be empty!\n"
@@ -48,8 +48,23 @@ if (! (/[A-Za-z ]{1,50}/.test(lastName))){
 }
 
 if (!(gender[0].checked || gender[1].checked || gender[2].checked)) {
-    message = "Please Select Your Gender";
+    message += "Please Select Your Gender\n";
 }
 
-window.alert(message)
+if (age === '0'){
+    message += "The age isn't chosen!\n"
+}
+
+const re_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+if(!re_email.test(String(email).toLowerCase())){
+    message += "Invalid email!\n"
+}
+
+const re_phone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+if(!re_phone.test(String(phone).toLowerCase())){
+    message += "Invalid phone!\n"
+}
+
+if(message)
+    window.alert(message)
 }
